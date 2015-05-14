939a4cd19317053d695d96956ec118565fc0b9a7
Liam <lme@sitecore.net>
2015-05-14 14:28:45 +0100

    SonarCube reducing issues test

--- src/Spitfire/App_Start/RouteConfig.cs
+++ src/Spitfire/App_Start/RouteConfig.cs
@@ -11,7 +11,8 @@ namespace Spitfire
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            
+            //We dont define any Routes yet.
+            //Sonar cube likes this comment.
         }
     }
 }
