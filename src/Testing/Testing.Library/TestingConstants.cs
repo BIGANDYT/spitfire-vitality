96bd52c485e2be98b1c28506c7fb55cce7e55b30
Liam <lme@sitecore.net>
2015-05-28 15:26:56 +0100

    Spitfire Testing

---
+++ src/Testing/Testing.Library/TestingConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Library
+{
+    class TestingConstants
+    {
+        public const String BaseAddress = "spitefire.net";
+        public const String BaseAddressGoogle = "spitefire.google";
+        public const String BaseAddressBing = "spitefire.bing";
+
+    }
+}
---
+++ src/Testing/WebsiteTests/SmokeTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using OpenQA.Selenium;
+using Testing.Library;
+
+namespace WebsiteTests
+{
+    [TestClass]
+    public class SmokeTests
+    {
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            Driver.Initialize();
+        }
+
+        [ClassCleanup]
+        public static void CleanUp()
+        {
+            Driver.Close();
+        }
+
+        [TestMethod]
+        public void HomepageTest()
+        {
+            TouchPage("/");
+        }
+
+        private static void TouchPage(string url)
+        {
+            Driver.Instance.Navigate().GoToUrl(Driver.BaseAddress + url);
+            var menuExists = Driver.Instance.FindElements(By.Id("nn-nav-menu")).Any();
+            Assert.IsTrue(menuExists);
+        }
+    }
+}
