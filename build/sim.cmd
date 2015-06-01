8fe5cf8bc3e9e9c1eec7780b8477aacedd98dc5f
Steve McGill <smg@sitecore.net>
2015-06-01 15:29:28 +0000

    CI: Don't move data to app_data, breaks build server strangly

--- build/sim.cmd
+++ build/sim.cmd
@@ -12,10 +12,5 @@ cd /D %InstallerPath%\SIM
 Hedgehog.Tds.Build.Sim.Console.exe install "InstanceName:%sitename%" "InstanceDirectory:%InstanceDirectory%" "RepoDirectory:%InstallerPath%" "RepoFile:%SitecoreInstallName%" "ConnectionString:%DbConnectionString%" "AppPoolIdentity:NetworkService" "LicencePath:%InstallerPath%\license.xml"
 robocopy %InstallerPath%\PostInstallFiles %InstanceDirectory%\%sitename%\Website /S /nfl /ndl
 
-:: Move Data folder to App_Data
-echo Renaming data folder to App_Data...
-rename %InstanceDirectory%\%sitename%\Data App_Data
-move %InstanceDirectory%\%sitename%\App_Data %InstanceDirectory%\%sitename%\Website\
-
 :End1
 cd %BuildDirectory%
