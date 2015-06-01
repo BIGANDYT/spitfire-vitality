2a8e3c17b790319c84f095d292a1cf294a5949b7
Steve McGill <smg@sitecore.net>
2015-06-01 21:34:50 +0100

    CI: BuildDirectory not dependant on SourceDirectory

--- build/vars.cmd
+++ build/vars.cmd
@@ -1,3 +1,4 @@
+IF NOT DEFINED BuildDirectory SET BuildDirectory=%CD%
 IF NOT DEFINED appcmd SET appcmd=CALL %WINDIR%\system32\inetsrv\appcmd
 IF NOT DEFINED SitecoreInstallName SET SitecoreInstallName=Sitecore 8.0 rev. 150427.zip
 IF NOT DEFINED BaseSite SET BaseSite=spitfire
@@ -6,5 +7,4 @@ IF NOT DEFINED InstanceDirectory SET InstanceDirectory=c:\websites
 IF NOT DEFINED DevDirectory SET DevDirectory=c:\websites\_dev
 IF NOT DEFINED DbConnectionString SET DbConnectionString=Data Source=.\SQLEXPRESS;User ID=sa;Password=10Philpot
 IF NOT DEFINED SourceDirectory SET SourceDirectory=%DevDirectory%\spitfiresource
-IF NOT DEFINED BuildDirectory SET BuildDirectory=%SourceDirectory%\build
 IF NOT DEFINED msbuild SET msbuild="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
