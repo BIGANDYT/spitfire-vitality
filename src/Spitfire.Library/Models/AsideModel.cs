9af8f824b14e6e091927c1add0c765c150343e52
Tingting_Zhu <tzh@sitecore.net>
2015-06-01 16:32:03 +0100

    Fix the scrolling bar issue; Aside Components - cssSubClass added ImageBanner removed class row.

--- src/Spitfire.Library/Models/AsideModel.cs
+++ src/Spitfire.Library/Models/AsideModel.cs
@@ -5,11 +5,12 @@
     public class AsideModel : StyleModel
     {
         public string Id { get; set; }
-
+        public string SubCssClass { get; private set; }
         public override void Initialize(Rendering rendering)
         {
             base.Initialize(rendering);
             Id = rendering.Parameters["Id"];
+            SubCssClass = rendering.Parameters["SubCssClass"];
         }
     }
 }
