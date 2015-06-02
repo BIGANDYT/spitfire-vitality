a94d03eb08c32bcecf9403de280dc8982caac213
Steve McGill <smg@sitecore.net>
2015-06-02 16:05:32 +0100

    CI: More export stuff, shrinking databases, etc.

--- build/export.cmd
+++ build/export.cmd
@@ -2,7 +2,21 @@
 call vars.cmd
 
 cd /D %InstallerPath%\SIM
-Hedgehog.Tds.Build.Sim.Console.exe export "InstanceName:%BaseSite%" "ExportFilePath:c:\websites\spitfire.zip"
+
+echo Purging EventQueue from Core...
+sqlcmd -S .\ -U "sa" -P "%DbSaPassword%" -d "%BaseSite%_Sitecore_core" -Q "DELETE FROM EventQueue"
+
+echo Purging History from Core...
+sqlcmd -S .\ -U "sa" -P "%DbSaPassword%" -d "%BaseSite%_Sitecore_core" -Q "DELETE FROM History"
+
+echo Shrinking databases...
+sqlcmd -S .\ -U "sa" -P "%DbSaPassword%" -d "master" -Q "DBCC SHRINKDATABASE(%BaseSite%_Sitecore_core)"
+sqlcmd -S .\ -U "sa" -P "%DbSaPassword%" -d "master" -Q "DBCC SHRINKDATABASE(%BaseSite%_Sitecore_master)"
+sqlcmd -S .\ -U "sa" -P "%DbSaPassword%" -d "master" -Q "DBCC SHRINKDATABASE(%BaseSite%_Sitecore_web)"
+sqlcmd -S .\ -U "sa" -P "%DbSaPassword%" -d "master" -Q "DBCC SHRINKDATABASE(%BaseSite%_Sitecore_reporting)"
+
+echo Exporting instance using SIM...
+Hedgehog.Tds.Build.Sim.Console.exe export "InstanceName:%BaseSite%" "ExportFilePath:%1"
 cd %BuildDirectory%
 
 pause
--- build/vars.cmd
+++ build/vars.cmd
@@ -5,6 +5,8 @@ IF NOT DEFINED BaseSite SET BaseSite=spitfire
 IF NOT DEFINED InstallerPath SET InstallerPath=c:\SpitfireInstaller
 IF NOT DEFINED InstanceDirectory SET InstanceDirectory=c:\websites
 IF NOT DEFINED DevDirectory SET DevDirectory=c:\websites\_dev
-IF NOT DEFINED DbConnectionString SET DbConnectionString=Data Source=.\SQLEXPRESS;User ID=sa;Password=10Philpot
+IF NOT DEFINED DbServer SET DbServer=.\SQLEXPRESS
+IF NOT DEFINED DbSaPassword SET DbSaPassword=10Philpot
+IF NOT DEFINED DbConnectionString SET DbConnectionString=Data Source=%DbServer%;User ID=sa;Password=%DbSaPassword%
 IF NOT DEFINED SourceDirectory SET SourceDirectory=%DevDirectory%\spitfiresource
 IF NOT DEFINED msbuild SET msbuild="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
