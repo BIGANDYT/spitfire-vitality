96d24a9eeda3b7ed16bd5b64cb5ef53c568bdfe7
Liam <lme@sitecore.net>
2015-06-02 11:39:32 +0100

    SectionPortfolio.cshtml stuff

---
+++ src/Spitfire.Library/Models/SectionPortfolioModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Spitfire.Library.Models
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Sitecore;
+    using Sitecore.Data.Items;
+    using Sitecore.Mvc.Presentation;
+
+    public class SectionPortfolioModel: IRenderingModel
+    {
+        public List<Item> Data { get; set; }
+
+        public void Initialize(Rendering rendering)
+        {
+            if (!string.IsNullOrEmpty(rendering.DataSource))
+            {
+                Item datasource = Context.Database.GetItem(rendering.DataSource);
+                Data = datasource.Children.OrderBy(x => x.Name).ToList();
+            }
+        }
+    }
+}
