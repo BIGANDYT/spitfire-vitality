4668124efb95a0507d8d252956b4dbb639e05897
Steve McGill <smg@sitecore.net>
2015-06-08 21:18:49 +0100

    CI - Don't check for admin privs on build server

--- build/install.cmd
+++ build/install.cmd
@@ -1,12 +1,14 @@
 @echo off
 cd /d %0\..
 
-:: Checking for administrator permissions...
-openfiles 1>nul 2>&1
-if not %errorlevel% equ 0 (
-    echo Please run this script as an administrator
-	pause
-	EXIT /B 1
+IF NOT DEFINED IsBuildServer (
+	:: Checking for administrator permissions...
+	openfiles 1>nul 2>&1
+	if not %errorlevel% equ 0 (
+		echo Please run this script as an administrator
+		pause
+		EXIT /B 1
+	)
 )
 
 call vars.cmd
