41ba019d8d30aa54a111aba9377100f948f665a8
Steve McGill <smg@sitecore.net>
2015-07-01 01:37:39 +0100

    Install WFFM using SIM

--- build/sim.cmd
+++ build/sim.cmd
@@ -2,7 +2,6 @@
 
 echo Installing %sitename% using SIM...
 cd /D %InstallerPath%\SIM
-Hedgehog.Tds.Build.Sim.Console.exe install "InstanceName:%sitename%" "InstanceDirectory:%InstanceDirectory%" "RepoDirectory:%InstallerPath%" "RepoFile:%SitecoreInstallName%" "ConnectionString:%DbConnectionString%" "AppPoolIdentity:NetworkService" "LicencePath:%InstallerPath%\license.xml"
-robocopy %InstallerPath%\PostInstallFiles %InstanceDirectory%\%sitename%\Website /S /nfl /ndl
+Hedgehog.Tds.Build.Sim.Console.exe install "InstanceName:%sitename%" "InstanceDirectory:%InstanceDirectory%" "RepoDirectory:%InstallerPath%" "RepoFile:%SitecoreInstallName%" "ConnectionString:%DbConnectionString%" "AppPoolIdentity:NetworkService" "LicencePath:%InstallerPath%\license.xml" "Modules:Spitfire PostInstall 1.0 rev. 000000.zip|Web Forms for Marketers 8.0 rev. 150625.zip"
 
 cd %BuildDirectory%
