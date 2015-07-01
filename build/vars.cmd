443b172493b01c5c2031647c391c483b53797b9f
Steve McGill <smg@sitecore.net>
2015-07-01 12:13:16 +0100

    Upgrade to Sitecore 8.0 rev. 150621 Add WFFM forms

--- build/vars.cmd
+++ build/vars.cmd
@@ -4,7 +4,7 @@ IF EXIST %CD%\vars.user.cmd (
 
 SET BuildDirectory=%CD%
 IF NOT DEFINED appcmd SET appcmd=CALL %WINDIR%\system32\inetsrv\appcmd
-IF NOT DEFINED SitecoreInstallName SET SitecoreInstallName=Sitecore 8.0 rev. 150427.zip
+IF NOT DEFINED SitecoreInstallName SET SitecoreInstallName=Sitecore 8.0 rev. 150621.zip
 IF NOT DEFINED SiteName SET SiteName=spitfire
 IF NOT DEFINED InstallerPath SET InstallerPath=c:\SpitfireInstaller
 IF NOT DEFINED InstanceDirectory SET InstanceDirectory=c:\websites
