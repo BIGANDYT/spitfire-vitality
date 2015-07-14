62ffb5b3fd8b00e324d5cd8f83ec9e584032c92d
Steve McGill <smg@sitecore.net>
2015-07-14 13:00:49 +0100

    Bunch of errorlevel checking to get the build to fail faster.

--- build/build.cmd
+++ build/build.cmd
@@ -4,25 +4,55 @@ cd /d %0\..
 call vars.cmd
 
 .\tools\NuGet.exe restore ..\Spitfire.sln
+IF "%ERRORLEVEL%" NEQ "0" (
+	echo Nuget Restore failed
+	exit /B %ERRORLEVEL%
+)
 
 if not exist ..\lib\Sitecore (
 	mkdir ..\lib\Sitecore
 	copy c:\websites\%SiteName%\Website\bin\Sitecore.*.dll ..\lib\Sitecore
+
+	IF "%ERRORLEVEL%" NEQ "0" (
+		echo Copying Sitecore libs failed
+		exit /B %ERRORLEVEL%
+	)
 )
 
 if not exist ..\lib\System (
 	mkdir ..\lib\System
 	copy c:\websites\%SiteName%\Website\bin\System.*.dll ..\lib\System
+
+	IF "%ERRORLEVEL%" NEQ "0" (
+		echo Copying System libs failed
+		exit /B %ERRORLEVEL%
+	)
 )
 
 SET DeployParameters=/p:DeployOnBuild=true /p:DeployDefaultTarget=WebPublish /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=False /p:publishUrl=%InstanceDirectory%\%sitename%\Website
 
 %msbuild% ..\src\Spitfire.Website\Spitfire.Website.csproj %DeployParameters%
+IF "%ERRORLEVEL%" NEQ "0" (
+	echo Build of Website project failed
+	exit /B %ERRORLEVEL%
+)
+
 %msbuild% ..\src\Spitfire.Website\Spitfire.Website.Static.csproj %DeployParameters%
+IF "%ERRORLEVEL%" NEQ "0" (
+	echo Build of Website static project failed
+	exit /B %ERRORLEVEL%
+)
 
 SET DevSettings=%InstanceDirectory%\%sitename%\Website\App_Config\Include\zSpitfire\DevSettings.config
 IF NOT EXIST %DevSettings% (
 	copy %DevSettings%.sample %DevSettings%
 	powershell -file build-FixDataFolder.ps1 "%DevSettings%" "%InstanceDirectory%\%sitename%\Data"
 	powershell -file build-FixUnicornFolder.ps1 "%DevSettings%" "%CD%"
+
+	IF "%ERRORLEVEL%" NEQ "0" (
+		echo Powershell updates failed
+		exit /B %ERRORLEVEL%
+	)
 )
+
+exit /B 0
