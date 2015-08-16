b721a023b2b39307d67733e8e2a053aeba052696
Steve McGill <smg@sitecore.net>
2015-08-16 11:39:04 +0100

    uncomment sync files :|

--- build/sync-installer.cmd
+++ build/sync-installer.cmd
@@ -3,5 +3,5 @@ cd /d %0\..
 
 ::call vars.cmd
 
-::tools\bash -c "tools/chmod 600 tools/sitecoreci.key"
-::tools\bash -c "tools/rsync --delete -zrtv -e 'tools\ssh -p 65422 -i tools/sitecoreci.key -o UserKnownHostsFile=./known_hosts' spitfireinstaller@sitecoreci.cloudapp.net:/cygdrive/c/spitfireinstaller/ /cygdrive/c/spitfireinstaller/"
+tools\bash -c "tools/chmod 600 tools/sitecoreci.key"
+tools\bash -c "tools/rsync --delete -zrtv -e 'tools\ssh -p 65422 -i tools/sitecoreci.key -o UserKnownHostsFile=./known_hosts' spitfireinstaller@sitecoreci.cloudapp.net:/cygdrive/c/spitfireinstaller/ /cygdrive/c/spitfireinstaller/"
