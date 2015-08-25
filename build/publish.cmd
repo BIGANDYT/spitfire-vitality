6f651d79c526f1746e9a15f2c75779e9a9b99224
Steve McGill <smg@sitecore.net>
2015-08-25 20:57:50 +0100

    Put Default.cshtml under Layouts folder Correct location in Sitecore Disable Ship publishing for now

--- build/publish.cmd
+++ build/publish.cmd
@@ -2,11 +2,12 @@
 cd /d %0\..
 call vars.cmd
 
-echo Smart publish...
-tools\curl -X POST --data '' "http://%sitename%/sitecore_ship/publish/smart"
-IF "%ERRORLEVEL%" NEQ "0" (
-	echo Package installation failed
-	exit /B %ERRORLEVEL%
-)
+:: Put back in but without Sitecore Ship
+::echo Smart publish...
+::tools\curl -X POST --data '' "http://%sitename%/sitecore_ship/publish/smart"
+::IF "%ERRORLEVEL%" NEQ "0" (
+::	echo Package installation failed
+::	exit /B %ERRORLEVEL%
+::)
 
 exit /B 0
