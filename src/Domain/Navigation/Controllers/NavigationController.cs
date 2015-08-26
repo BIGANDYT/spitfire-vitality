933dce0a34aee967d52201ea4d48ae3b8955a6e5
Thomas Eldblom <the@sitecore.net>
2015-08-26 22:52:42 +1000

    Added primary navigation

--- src/Domain/Navigation/Controllers/NavigationController.cs
+++ src/Domain/Navigation/Controllers/NavigationController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Sitecore;
 using Sitecore.Data.Items;
 using Sitecore.Mvc.Controllers;
 using Sitecore.Mvc.Presentation;
@@ -12,11 +13,13 @@ namespace Spitfire.Navigation.Controllers
 
     public class NavigationController : Controller
     {
+        private Item _navigationRoot;
+
         public ActionResult Breadcrumb()
         {
             var items = new NavigationItems()
             {
-                Items = this.GetNavigableItems().Reverse(),
+                Items = this.GetNavigationHierarchy().Reverse(),
             };
 
             items.ActiveItem = items.Items.LastOrDefault();
@@ -24,7 +27,7 @@ namespace Spitfire.Navigation.Controllers
             return this.View("Breadcrumb", items);
         }
 
-        private IEnumerable<Item> GetNavigableItems()
+        private IEnumerable<Item> GetNavigationHierarchy()
         {
             var item = RenderingContext.Current.Rendering.Item;
             while (item != null)
@@ -39,9 +42,39 @@ namespace Spitfire.Navigation.Controllers
 
         public ActionResult PrimaryMenu()
         {
-            //TODO: Find _NavigationRoot and return the correct NavigationItems
-            var items = new NavigationItems();
+            var items = new NavigationItems()
+            {
+                Items = this.GetPrimaryNavigationItems(),
+                ActiveItem = this.GetActiveNavigationItem(),
+            };
+
+            items.ActiveItem = items.Items.LastOrDefault();
             return this.View("PrimaryMenu", items);
+        }
+
+        private Item GetActiveNavigationItem()
+        {
+            //TODO
+            return null;
+        }
+
+        private IEnumerable<Item> GetPrimaryNavigationItems()
+        {
+            var items = new List<Item>();
+            if (MainUtil.GetBool(this.NavigationRoot[Templates.NavigationRoot.Fields.IncludeRootInPrimaryMenu], false))
+            {
+                if (this.NavigationRoot.IsDerived(Templates.Navigable.ID))
+                {
+                    items.Add(this.NavigationRoot);
+                }
+            }
+            items.AddRange(this.NavigationRoot.Children.Where(i => i.IsDerived(Templates.Navigable.ID)));
+            return items;
+        }
+
+        public Item NavigationRoot
+        {
+            get { return this._navigationRoot ?? (this._navigationRoot = NavigationRootService.GetNavigationRoot(RenderingContext.Current.Rendering.Item)); }
         }
 
         public ActionResult SecondaryMenu()
